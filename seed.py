7bd7aa79acfee517d408fb969d435267dad089dd
Craig Omore <craiggomore@gmail.com>
2026-01-15 09:31:55 +0300

    adds content to seed.py

--- seed.py
+++ seed.py
@@ -21,10 +21,31 @@ with app.app_context():
     db.drop_all()
     db.create_all()
 
- # ---------- USERS ----------
+# ---------- USERS ----------
     volunteer = User(
         name="Craig Volunteer",
         email="volunteer@test.com",
         password=generate_password_hash("password123"),
         role="volunteer"
     )
+
+    org_owner = User(
+        name="Org Owner",
+        email="org@test.com",
+        password=generate_password_hash("password123"),
+        role="organization"
+    )
+
+    db.session.add_all([volunteer, org_owner])
+    db.session.commit()
+
+  # ---------- ORGANIZATION ----------
+    org = Organization(
+        name="Helping Hands",
+        description="Community volunteer organization",
+        location="Nairobi",
+        owner_id=org_owner.id
+    )
+
+    db.session.add(org)
+    db.session.commit()
