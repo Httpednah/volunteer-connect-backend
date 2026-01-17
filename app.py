d1fb67a2d65109b5ffdab691e2bf477c97379537
Ednah wairimu kimani <kimaniednah@gmail.com>
2026-01-17 08:39:37 +0300

    extentions line

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+from extensions import db 
 
 # --------------------
 # App setup
